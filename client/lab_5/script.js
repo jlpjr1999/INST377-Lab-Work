007188ae0d669aa00ddc4b254418e30eefd094a8
jlpjr1999 <jpoindexter00@gmail.com>
2023-03-03 21:18:25 -0500

    completed lab. javascript follows lecture video

--- client/lab_5/script.js
+++ client/lab_5/script.js
@@ -12,11 +12,17 @@ function filterList(list, query) {
 
     Ask the TAs if you need help with this
   */
+  return list.filter((item) => {
+    const lowerCaseName = item.name.toLowerCase();
+    const lowerCaseQuery = query.toLowerCase();
+    return lowerCaseName.includes(lowerCaseQuery);
+  });
 }
 
 async function mainEvent() { // the async keyword means we can make API requests
   const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
   // Add a querySelector that targets your filter button here
+  const filterButton = document.querySelector('.filter_button');
 
   let currentList = []; // this is "scoped" to the main event function
   
@@ -66,6 +72,18 @@ async function mainEvent() { // the async keyword means we can make API requests
     Fire it here and filter for the word "pizza"
     you should get approximately 46 results
   */
+  filterButton.addEventListener('click', (event) => {
+    console.log("clicked filter button");
+
+    const formData = new FormData(mainForm);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+
+    const newList = filterList(currentList, formProps.resto);
+
+    console.log(newList);
+  });
 }
 
 /*
