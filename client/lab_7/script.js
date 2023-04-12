254c966555bdd1f097ec4e2ba422b45997f07345
jlpjr1999 <jpoindexter00@gmail.com>
2023-04-11 21:43:15 -0400

    part 1 of lab 8 done

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -61,8 +61,6 @@ function getRandomIntInclusive(min, max) {
 
   async function mainEvent() { // the async keyword means we can make API requests
     const mainForm = document.querySelector('.main_form'); // This class name needs to be set on your form before you can listen for an event on it
-    // Add a querySelector that targets your filter button here
-    const filterButton = document.querySelector('#filter');
     const loadDataButton = document.querySelector('#data_load');
     const generateListButton = document.querySelector('#generate');
     const textField = document.querySelector('#resto');
@@ -71,8 +69,15 @@ function getRandomIntInclusive(min, max) {
     loadAnimation.style.display = 'none';
 
     generateListButton.classList.add('hidden');
+
+    
+    const storedData = localStorage.getItem('storedData');
+    const parsedData = JSON.parse(storedData);
+    if (parsedData.length > 0) {
+      generateListButton.classList.remove('hidden');
+
+    }
   
-    let storedList = [];
     let currentList = []; // this is "scoped" to the main event function
     
     loadDataButton.addEventListener('click', async (submitEvent) => { // async has to be declared on every function that needs to "await" something    
@@ -83,32 +88,17 @@ function getRandomIntInclusive(min, max) {
       const results = await fetch('https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json');
   
       // This changes the response from the GET into data we can use - an "object"
-      storedList = await results.json();
-      if (storedList.length > 0) {
-        generateListButton.classList.remove('hidden');
-      };
+      const storedList = await results.json();
+      localStorage.setItem('storedData', JSON.stringify(storedList));
   
       loadAnimation.style.display = 'none';
   
       console.table(storedList); 
     });
 
-    filterButton.addEventListener('click', (event) => {
-      console.log("clicked filter button");
-  
-      const formData = new FormData(mainForm);
-      const formProps = Object.fromEntries(formData);
-  
-      console.log(formProps);
-  
-      const newList = filterList(storedList, formProps.resto);
-      injectHTML(newList);
-      console.log(newList);
-    });
-  
     generateListButton.addEventListener("click", (event) => {
       console.log("generate new list");
-      currentList = cutRestaurantList(storedList);
+      currentList = cutRestaurantList(parsedData);
       console.log(currentList);
       injectHTML(currentList);
     });
